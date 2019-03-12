fcd696be62348a69fd3d07d6d50fc992f01ed895
Ghanshyam Thakkar <ghanshyamthakkardev@gmail.com>
2019-03-12 15:29:33 +0530

    Initial Commit

---
+++ precache-manifest.b13fa5be3967f6adb92668ff583c1bda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ccc622ab0412e63e4b5f",
+    "url": "/static/js/main.2370759f.chunk.js"
+  },
+  {
+    "revision": "ef31f7a7817becd69e17",
+    "url": "/static/js/2.531fa83c.chunk.js"
+  },
+  {
+    "revision": "ccc622ab0412e63e4b5f",
+    "url": "/static/css/main.e15d7b6d.chunk.css"
+  },
+  {
+    "revision": "ef31f7a7817becd69e17",
+    "url": "/static/css/2.e1199f73.chunk.css"
+  },
+  {
+    "revision": "35d801295e658bf816171e4fee303724",
+    "url": "/index.html"
+  }
+];
